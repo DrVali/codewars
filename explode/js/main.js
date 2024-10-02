d5ef5cf33c5eca364bb3a35cc089754b929da15a
DrVali <ahmadi.world@gmail.com>
2024-10-02 13:42:08 +0330

    Create main.js

---
+++ explode/js/main.js
@@ -0,0 +1,23 @@
+function explode(x){
+   
+    let score;
+
+    if (typeof x[0] === 'number' && typeof x[1] === 'number') {
+        score = x[0] + x[1];
+    } 
+    else if (typeof x[0] === 'number') {
+        score = x[0];
+    } 
+    else if (typeof x[1] === 'number') {
+        score = x[1];
+    } 
+    else {
+        return 'Void!';
+    }
+    
+    let result = Array(score).fill(x);
+    return result;
+
+}
+
+
